7155c491237fca7fe4b2050431e1379db768f839
Deran Decker <me@derandecker.com>
2020-05-26 21:17:51 -1000

    Add NetworkUtils, JSONUtils, Recipe POJO, and Room dependencies

---
+++ app/src/main/java/com/derandecker/bakingapp/model/Recipe.java
@@ -0,0 +1,51 @@
+package com.derandecker.bakingapp.model;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Recipes")
+public class Recipe {
+    @PrimaryKey
+    private int id;
+    private String name;
+
+
+    @Ignore
+    public Recipe() {
+    }
+
+    public Recipe(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public class Ingredients {
+
+    }
+
+    public class Steps {
+
+    }
+}
+
+//can try embedded but probably need to do a new pojo for ingredients and steps
+//so can account for differing number of ingredients and steps in each recipe
+
+//public class Address {
+//    public String street;
+//    public String state;
+//    public String city;
+//
+//    @ColumnInfo(name = "post_code") public int postCode;
+//}
+//
+//@Entity
+//public class User {
+//    @PrimaryKey public int id;
+//
+//    public String firstName;
+//
+//    @Embedded public Address address;
+//}
---
+++ app/src/main/java/com/derandecker/bakingapp/utils/JSONUtils.java
@@ -0,0 +1,31 @@
+package com.derandecker.bakingapp.utils;
+
+import com.derandecker.bakingapp.model.Recipe;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONUtils {
+
+    public static List<Recipe> parseRecipesJson(String json) throws JSONException {
+
+        JSONArray recipesJson = new JSONArray(json);
+        Recipe currentRecipe;
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+
+        for (int i = 0; i < recipesJson.length(); i++) {
+            JSONObject recipe = recipesJson.getJSONObject(i);
+            int id = recipe.getInt("id");
+            String name = recipe.getString("name");
+
+            currentRecipe = new Recipe(id, name);
+            recipes.add(currentRecipe);
+        }
+        return recipes;
+    }
+
+}
