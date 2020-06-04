e1762288c08a4d9044b8c7c19caae99717e4d464
Deran Decker <me@derandecker.com>
2020-06-04 03:07:13 -1000

    Add POJO for Ingredients and RecipeWithIngredients

---
+++ app/src/main/java/com/derandecker/bakingapp/model/Ingredients.java
@@ -0,0 +1,29 @@
+package com.derandecker.bakingapp.model;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Ingredients {
+    @PrimaryKey
+    public int ingredientsId;
+    public int recipeId;
+    public int quantity;
+    public String measure;
+    public String ingredient;
+
+    @Ignore
+    public Ingredients() {
+    }
+
+    public Ingredients(int ingredientsId, int recipeId, int quantity,
+                       String measure, String ingredient) {
+        this.ingredientsId = ingredientsId;
+        this.recipeId = recipeId;
+        this.quantity = quantity;
+        this.measure = measure;
+        this.ingredient = ingredient;
+    }
+
+}
--- app/src/main/java/com/derandecker/bakingapp/model/Recipe.java
+++ app/src/main/java/com/derandecker/bakingapp/model/Recipe.java
@@ -7,53 +7,25 @@ import androidx.room.PrimaryKey;
 @Entity(tableName = "Recipes")
 public class Recipe {
     @PrimaryKey
-    private int id;
-    private String name;
+    public int recipeId;
+    public String name;
 
 
     @Ignore
     public Recipe() {
     }
 
-    public Recipe(int id, String name){
-        this.id = id;
+    public Recipe(int id, String name) {
+        this.recipeId = id;
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getId(){
-        return id;
-    }
-
-
-    public class Ingredients {
-
-    }
-
-    public class Steps {
-
+    public int getId() {
+        return recipeId;
     }
 }
 
-//can try embedded but probably need to do a new pojo for ingredients and steps
-//so can account for differing number of ingredients and steps in each recipe
-
-//public class Address {
-//    public String street;
-//    public String state;
-//    public String city;
-//
-//    @ColumnInfo(name = "post_code") public int postCode;
-//}
-//
-//@Entity
-//public class User {
-//    @PrimaryKey public int id;
-//
-//    public String firstName;
-//
-//    @Embedded public Address address;
-//}
